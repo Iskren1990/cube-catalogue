1c15096999efaa47a55ea984985ef98ca58d9b93
Iskren1990 <Iskren.1990@abv.bg>
2020-10-03 15:20:07 +0300

    added views

--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,13 @@
 // TODO: Require Controllers...
+const {/*import all cubes func*/} = require("../controllers/cube");
 
 module.exports = (app) => {
     // TODO...
+    app.get("/", function(req, res) { res.render("index")})
+    app.get("/about", function(req, res) { res.render("about")})
+    app.get("/create", function(req, res) { res.render("create")})
+    app.get("/add-accessory", function(req, res) { res.render("createAccessory")})
+    app.get("/details/:id", function(req, res) { res.render("details")})
+
+    app.use("*", function(req, res) { res.render("404")})
 };
