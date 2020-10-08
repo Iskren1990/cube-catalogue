83c8c909335242cb8496899048be2a641a38b1e2
Iskren1990 <Iskren.1990@abv.bg>
2020-10-08 21:46:56 +0300

    Heroku tests

--- config/data.js
+++ config/data.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 module.exports = function (config) {
     console.log(config);
     mongoose.connect(
-        config.dbUri,
+        config.dbUri || "mongodb+srv://test:test@cluster0.4dqi4.mongodb.net/cube-catalog?retryWrites=true&w=majority",
         config.dbOptions,
         function (err, suc) {
             if (err) { console.log("Database Error: ", err) }
